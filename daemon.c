22a94a118940a647810ccb0ba6f4982a794c9989
Peter Hofmann <scm@uninformativ.de>
2015-09-20 20:01:29 +0200

    Always use c_background as the window's bg

--- daemon.c
+++ daemon.c
@@ -56,14 +56,15 @@ setup_css(void)
     GtkCssProvider *provider = gtk_css_provider_new();
     GdkDisplay *display = gdk_display_get_default();
     GdkScreen *screen = gdk_display_get_default_screen(display);
+    gchar *css;
 
+    css = g_strdup_printf("GtkWindow { background-color: %s; }", c_background);
     gtk_style_context_add_provider_for_screen(screen,
             GTK_STYLE_PROVIDER(provider),
             GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    gtk_css_provider_load_from_data(provider,
-                                    "GtkWindow { background-color: " BG_COLOR "; }",
-                                    -1, NULL);
+    gtk_css_provider_load_from_data(provider, css, -1, NULL);
     g_object_unref(provider);
+    g_free(css);
 }
 
 gboolean
