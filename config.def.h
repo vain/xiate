45e1184002fd4a23f207e3d2179f27a9600fcd77
Peter Hofmann <scm@uninformativ.de>
2015-09-22 20:00:48 +0200

    config.def.h: CGA palette and DejaVu Sans Mono

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 char     *args_default[]   = { "/bin/bash", "-l", "-i", NULL };
 gboolean  enable_bold      = FALSE;
-char     *font_default     = "Ttyp0 10";
+char     *font_default     = "DejaVu Sans Mono 9";
 gint      internal_border  = 2;
 guint     scrollback_lines = 50000;
 
@@ -11,21 +11,21 @@ char *c_bold = "#FFFFFF";
 char *c_palette[16] = {
     /* Dark */
     /* Black */   "#000000",
-    /* Red */     "#FF6565",
-    /* Green */   "#93D44F",
-    /* Yellow */  "#EAB93D",
-    /* Blue */    "#204A87",
-    /* Magenta */ "#CE5C00",
-    /* Cyan */    "#89B6E2",
-    /* White */   "#CCCCCC",
+    /* Red */     "#AA0000",
+    /* Green */   "#00AA00",
+    /* Yellow */  "#AA5500",
+    /* Blue */    "#0000AA",
+    /* Magenta */ "#AA00AA",
+    /* Cyan */    "#00AAAA",
+    /* White */   "#AAAAAA",
 
     /* Bright */
-    /* Black */   "#555753",
-    /* Red */     "#CF0000",
-    /* Green */   "#00CF00",
-    /* Yellow */  "#FFC123",
-    /* Blue */    "#3465A4",
-    /* Magenta */ "#F57900",
-    /* Cyan */    "#46A4FF",
+    /* Black */   "#555555",
+    /* Red */     "#FF5555",
+    /* Green */   "#55FF55",
+    /* Yellow */  "#FFFF55",
+    /* Blue */    "#5555FF",
+    /* Magenta */ "#FF55FF",
+    /* Cyan */    "#55FFFF",
     /* White */   "#FFFFFF",
 };
