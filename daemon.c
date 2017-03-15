8090bb21665ab2b67249be01124c2c0666fa77ab
Cesar Crusius <ccrusius@cisco.com>
2017-03-14 20:52:29 -0700

    Standard shortcuts for increasing and decreasing font size.

--- daemon.c
+++ daemon.c
@@ -261,6 +261,13 @@ sig_key_press(GtkWidget *widget, GdkEvent *event, gpointer data)
             case GDK_KEY_V:
                 vte_terminal_paste_clipboard(term);
                 return TRUE;
+            case GDK_KEY_plus:
+            case GDK_KEY_equal:
+                sig_increase_font_size(term, data);
+                return TRUE;
+            case GDK_KEY_minus:
+                sig_decrease_font_size(term, data);
+                return TRUE;
             case GDK_KEY_KP_0:
                 vte_terminal_set_font_scale(term, 1);
                 vte_terminal_set_geometry_hints_for_window(VTE_TERMINAL(term),
